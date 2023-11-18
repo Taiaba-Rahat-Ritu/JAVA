64da2813b05388e86e4e46a46bca12068b8c7899
Taiaba-Rahat-Ritu <taiabarahatritu@gmail.com>
2023-11-19 02:19:28 +0600

    first commit

---
+++ src/ConnectionManager.java
@@ -0,0 +1,23 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+
+    private static final String URL = "jdbc:mysql://localhost:3307/javaRegistration";
+    private static final String USER = "root";
+/*private static final String PASSWORD = ""; */
+
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Failed to load MySQL JDBC driver", e);
+        }
+
+        return DriverManager.getConnection(URL, USER, "");
+    }
+}
+
